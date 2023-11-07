6c558989b2f63c117ead36a802faec0efc413b40
patelz6 <122781134+patelz6@users.noreply.github.com>
2023-11-07 17:01:52 +0000

    Added Planet class for planet bodies

---
+++ Planet.java
@@ -0,0 +1,22 @@
+public class Planet{
+    protected double distance;
+    protected double diameter;
+    protected double angle;
+    protected String color;
+    
+    protected double speed;
+
+    public Planet(double distance, double angle, double diameter, String color){
+        this.distance = distance;
+        this.angle = angle;
+        this.diameter = diameter;
+        this.color = color;
+        
+    }
+
+  
+
+    
+
+    
+}
