115d88e54c7581c86220dc620b240b56e24a6924
patelz6 <122781134+patelz6@users.noreply.github.com>
2023-11-07 17:09:01 +0000

    Added accessors / mutators for all attributes of Planet

--- Planet.java
+++ Planet.java
@@ -13,6 +13,51 @@ public class Planet{
         this.color = color;
         this.solarSystem = solarSystem;
     }
+     public double getAngle() {
+        return angle;
+    }
+
+    public void setAngle(double angle) {
+        this.angle = angle;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(double diameter) {
+        this.diameter = diameter;
+    }
+
+    public SolarSystem getSolarSystem() {
+        return solarSystem;
+    }
+
+    public void setSolarSystem(SolarSystem solarSystem) {
+        this.solarSystem = solarSystem;
+    }
+
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public void movePlanet(double speed){
+        angle = angle + speed;
+        
+    }
 
   
 
