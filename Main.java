5dedff7d45b50a6d81884e2d17675d82df78428e
patelz6 <122781134+patelz6@users.noreply.github.com>
2023-11-07 19:06:51 +0000

    Added Sun object in Main.java

--- Main.java
+++ Main.java
@@ -2,10 +2,15 @@ public class Main {
     public static void main(String args[]){
         
         SolarSystem solarSystem = new SolarSystem(1250, 1250);
+        
+        Sun sun = new Sun(0, 0, 75, "YELLOW", solarSystem, true);
+        
         Planet earth = new Planet(150, 0, 35, "BLUE", solarSystem);
             Moon moon = new Moon(30, 0, 15, "WHITE", solarSystem, earth.getDistance(), earth.getAngle(), earth);
 
         while (true) {
+            sun.drawSun();
+            
             earth.drawPlanet();
             earth.movePlanet(1);
                 moon.drawMoon();
