de2ed26adc0870100970a443f46aff2c8bb9c541
patelz6 <122781134+patelz6@users.noreply.github.com>
2023-11-07 17:06:16 +0000

    Added earth and its movement
    Note that the way of movement is not following OOP principles.

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
 
 public class Main {
+    
     public static void main(String args[]){
         
         SolarSystem solarSystem = new SolarSystem(1250, 1250);
 
-       
+        while (true) {
+           int pos = 0;
+            solarSystem.drawSolarObject(100, pos, 30, "BLUE");
+            pos++;
+            solarSystem.finishedDrawing();
+        }
     }
 }
