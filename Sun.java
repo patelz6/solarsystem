7de4509e26d24352457105e0eb337cee60ce6771
patelz6 <122781134+patelz6@users.noreply.github.com>
2023-11-07 18:35:14 +0000

    Added Sun.java class for Suns

---
+++ Sun.java
@@ -0,0 +1,14 @@
+public class Sun extends Planet{
+    private boolean isSun;
+
+    public Sun(double distance, double angle, double diameter, String color, SolarSystem solarSystem, boolean isSun){
+        super(distance, angle, diameter, color, solarSystem);
+        this.isSun = isSun;
+        if (isSun = true) 
+        {
+            distance = 0;
+            angle = 0;
+            color = "YELLOW";
+        }
+    }
+}
