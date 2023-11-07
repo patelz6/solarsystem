16b299bf70cd48556ca5d287827b9fa688565918
patelz6 <122781134+patelz6@users.noreply.github.com>
2023-11-07 17:13:02 +0000

    Obeyed OOP principles to move planets with example Earth
    Forgot to mention, also added multiplicative variable speeds for moving planets in previous commit. No local variables in Main.java.

--- Main.java
+++ Main.java
@@ -1,14 +1,12 @@
-
 public class Main {
-    
     public static void main(String args[]){
         
         SolarSystem solarSystem = new SolarSystem(1250, 1250);
+        Planet earth = new Planet(150, 0, 35, "BLUE", solarSystem);
 
         while (true) {
-           int pos = 0;
-            solarSystem.drawSolarObject(100, pos, 30, "BLUE");
-            pos++;
+            earth.drawPlanet();
+            earth.movePlanet(1);
             solarSystem.finishedDrawing();
         }
     }
