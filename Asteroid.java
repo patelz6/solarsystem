39839fc290774a35729207c50f7d186ecd105ff3
patelz6 <122781134+patelz6@users.noreply.github.com>
2023-11-07 20:37:12 +0000

    Added random numbers
    Added random instances and random numbers for future asteroidLoop method for randomised asteroid generation.

--- Asteroid.java
+++ Asteroid.java
@@ -1,10 +1,18 @@
+import java.util.Random;
+
 public class Asteroid extends Planet{
     
     private int asteroidCount;
     private double speed;
-
     
-
+    Random random1 = new Random();
+    Random random2 = new Random();
+    Random random3 = new Random();
+    
+    int randomNumber = random1.nextInt(101) + 200;
+    double randomSize = random2.nextInt(11) - 5;
+    double randomRadian = random3.nextDouble() * 360 - 180;
+    
     public Asteroid(double distance, double angle, double diameter, String color, SolarSystem solarSystem, int asteroidCount){
         super(distance, angle, diameter, color, solarSystem);
         this.asteroidCount = asteroidCount;
