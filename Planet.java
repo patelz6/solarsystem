802d0bb255c7d3f7e7bd3c332ab6d5af158b907f
patelz6 <122781134+patelz6@users.noreply.github.com>
2023-11-07 17:08:14 +0000

    Added Solar system marker for every Planet
    Also kept protected visibility for future inheritance

--- Planet.java
+++ Planet.java
@@ -3,15 +3,15 @@ public class Planet{
     protected double diameter;
     protected double angle;
     protected String color;
-    
+    protected SolarSystem solarSystem;
     protected double speed;
 
-    public Planet(double distance, double angle, double diameter, String color){
+    public Planet(double distance, double angle, double diameter, String color, SolarSystem solarSystem){
         this.distance = distance;
         this.angle = angle;
         this.diameter = diameter;
         this.color = color;
-        
+        this.solarSystem = solarSystem;
     }
 
   
