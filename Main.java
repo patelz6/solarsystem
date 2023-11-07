bfaa3347ec27108679a19b38600ae56bd5e6df40
patelz6 <122781134+patelz6@users.noreply.github.com>
2023-11-07 17:42:12 +0000

    Added moon object
    Added moon object to test drawmoon / moveMoon

--- Main.java
+++ Main.java
@@ -3,10 +3,14 @@ public class Main {
         
         SolarSystem solarSystem = new SolarSystem(1250, 1250);
         Planet earth = new Planet(150, 0, 35, "BLUE", solarSystem);
+            Moon moon = new Moon(30, 0, 15, "WHITE", solarSystem, earth.getDistance(), earth.getAngle(), earth);
 
         while (true) {
             earth.drawPlanet();
             earth.movePlanet(1);
+                moon.drawMoon();
+                moon.moveMoon(2);
+            
             solarSystem.finishedDrawing();
         }
     }
