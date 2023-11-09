6cb4de23366d1241949f2e8bd738ca336e2e14aa
patelz6 <122781134+patelz6@users.noreply.github.com>
2023-11-09 17:18:37 +0000

    javadoc for Asteroid.java

--- Asteroid.java
+++ Asteroid.java
@@ -1,6 +1,10 @@
 import java.util.Random;
 
-public class Asteroid extends Planet{
+/**
+ * The Asteroid class represents an asteroid in the solar system.
+ * It extends the Planet class and adds additional functionality for asteroids.
+ */
+public class Asteroid extends Planet {
     
     private int asteroidCount;
     private double speed;
@@ -9,29 +13,63 @@ public class Asteroid extends Planet{
     Random random2 = new Random();
     Random random3 = new Random();
 
+    /**
+     * Constructs a new Asteroid object.
+     *
+     * @param distance The distance of the asteroid from the sun.
+     * @param angle The angle of the asteroid.
+     * @param diameter The diameter of the asteroid.
+     * @param color The color of the asteroid.
+     * @param solarSystem The solar system the asteroid belongs to.
+     * @param asteroidCount The number of asteroids.
+     */
     public Asteroid(double distance, double angle, double diameter, String color, SolarSystem solarSystem, int asteroidCount){
         super(distance, angle, diameter, color, solarSystem);
         this.asteroidCount = asteroidCount;
     }
 
+    /**
+     * Moves the asteroid by a certain speed.
+     *
+     * @param speed The speed to move the asteroid by.
+     */
     public void moveAsteroid(double speed){
         angle = angle + speed;
     }
 
+    /**
+     * Draws the asteroid in the solar system.
+     */
     public void drawAsteroid(){
         solarSystem.drawSolarObject(distance, angle, diameter, color);
     }
     
+    /**
+     * Returns the angle of the asteroid.
+     *
+     * @return The angle of the asteroid.
+     */
     public double getAngle() {
         return angle;
     }
 
+    /**
+     * Sets the angle of the asteroid.
+     *
+     * @param angle The new angle of the asteroid.
+     */
     public void setAngle(double angle) {
         this.angle = angle;
     }
 
     
     
+    /**
+     * Loops through the specified number of asteroids and draws them in the solar system.
+     * Each asteroid is drawn at a random distance and angle, and with a random size.
+     *
+     * @param asteroidCount The number of asteroids to draw.
+     */
     public void asteroidLoop(int asteroidCount){
         for(int i = 0; i < asteroidCount; i++){
             
@@ -40,8 +78,9 @@ public class Asteroid extends Planet{
             double randomRadian = random3.nextDouble() * 360 - 180;
             
             solarSystem.drawSolarObject(randomNumber, randomRadian-angle, randomSize, color);
-            
-            
         }
+    }  
+}
+
     }  
 }
