b7ec66517908f544ef20cd5f9e14d3f0137179ca
patelz6 <122781134+patelz6@users.noreply.github.com>
2023-11-09 17:20:33 +0000

    commenting through for clarity and javadoc

--- Sun.java
+++ Sun.java
@@ -1,6 +1,20 @@
+/**
+ * The Sun class represents the sun in the solar system.
+ * It extends the Planet class and adds additional functionality for the sun.
+ */
 public class Sun extends Planet{
     private boolean isSun;
 
+    /**
+     * Constructs a new Sun object.
+     *
+     * @param distance The distance of the sun from the center of the solar system.
+     * @param angle The angle of the sun.
+     * @param diameter The diameter of the sun.
+     * @param color The color of the sun.
+     * @param solarSystem The solar system the sun belongs to.
+     * @param isSun A boolean indicating if the object is the sun.
+     */
     public Sun(double distance, double angle, double diameter, String color, SolarSystem solarSystem, boolean isSun){
         super(distance, angle, diameter, color, solarSystem);
         this.isSun = isSun;
@@ -10,10 +24,13 @@ public class Sun extends Planet{
             angle = 0;
             color = "YELLOW";
         }
-        public void drawSun()
+    }
+
+    /**
+     * Draws the sun in the solar system.
+     */
+    public void drawSun()
     {
         solarSystem.drawSolarObject(distance, angle, diameter, color);
-        
-    }
     }
 }
